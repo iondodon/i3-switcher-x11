4d23bf25eafde0416893615495f6804b8e66bdb6
Ion Dodon <iondodon2@gmail.com>
2024-02-13 12:40:42 +0200

    Added thread for the UI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use gtk::prelude::{ApplicationExt, ApplicationExtManual, ButtonExt, ContainerExt, WidgetExt};
 use gtk::{Application, ApplicationWindow, Button};
 use i3ipc::{reply::NodeType, I3Connection};
@@ -30,28 +29,30 @@ fn print_window_names(node: &Node) {
 }
 
 fn main() {
-    let application = Application::builder()
+    thread::spawn(|| {
+        let application = Application::builder()
         .application_id("com.example.FirstGtkApp")
         .build();
 
-    application.connect_activate(|app| {
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .title("First GTK Program")
-            .default_width(350)
-            .default_height(70)
-            .build();
+        application.connect_activate(|app| {
+            let window = ApplicationWindow::builder()
+                .application(app)
+                .title("First GTK Program")
+                .default_width(350)
+                .default_height(70)
+                .build();
 
-        let button = Button::with_label("Click me!");
-        button.connect_clicked(|_| {
-            eprintln!("Clicked!");
+            let button = Button::with_label("Click me!");
+            button.connect_clicked(|_| {
+                eprintln!("Clicked!");
+            });
+            window.add(&button);
+
+            window.show_all();
         });
-        window.add(&button);
 
-        window.show_all();
+        application.run();
     });
-
-    application.run();
     
     // Establish a connection to the i3 IPC interface
     let mut connection = I3Connection::connect().unwrap();
