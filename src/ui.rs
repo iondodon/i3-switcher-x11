8a03eddfe3d9cce7186cccde63856fbf2afcd64c
Ion Dodon <iondodon2@gmail.com>
2024-02-15 18:01:42 +0200

    Move window to center

--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,5 @@
 use gdk4::glib::{self, clone};
+use gtk4::gsk;
 use gtk4::prelude::ButtonExt;
 use gtk4::prelude::WidgetExt;
 use gtk4::prelude::BoxExt;
@@ -16,59 +17,89 @@ use gtk4::glib::ControlFlow;
 
 pub fn setup(app: &Application, i3_conn: Arc<Mutex<I3Connection>>, is_visible: Arc<AtomicBool>) {
     let window = ApplicationWindow::builder()
-            .application(app)
-            .title("First GTK Program")
-            .default_width(350)
-            .default_height(70)
-            .build();
+        .application(app)
+        .title("First GTK Program")
+        .hexpand_set(true)
+        .hexpand(true)
+        .build();
 
-        let controller = EventControllerKey::new();
-        let window_clone = window.clone();
-        let is_visible_clone = is_visible.clone();
-        controller.connect_key_released(move |_, keyval, _, _| {
-            match keyval.name().unwrap().as_str() {
-                "Alt_L" => { 
-                    window_clone.hide(); 
-                    println!("Alt released gtk");
-                    is_visible_clone.store(false, Ordering::SeqCst);
-                },
-                _ => {}
-            }
-        });
-        window.add_controller(controller);
-        window.present();
-        window.hide();
+    let c = window.css_classes();
+    println!("{:?}", c);
 
-        update_window_content(&window, i3_conn.clone());
+    let controller = EventControllerKey::new();
+    let window_clone = window.clone();
+    let is_visible_clone = is_visible.clone();
+    controller.connect_key_released(move |_, keyval, _, _| {
+        match keyval.name().unwrap().as_str() {
+            "Alt_L" => { 
+                window_clone.hide(); 
+                println!("Alt released gtk");
+                is_visible_clone.store(false, Ordering::SeqCst);
+            },
+            _ => {}
+        }
+    });
+    window.add_controller(controller);
+    window.present();
+    window.hide();
 
-        let is_visible_clone = is_visible.clone();
-        let i3_conn_clone = i3_conn.clone();
-        glib::timeout_add_local(Duration::from_millis(100), clone!(@weak window => @default-return ControlFlow::Continue, move || {
-            println!("Now is {}", is_visible_clone.load(Ordering::SeqCst));
-            if is_visible_clone.load(Ordering::SeqCst) {
-                update_window_content(&window, i3_conn_clone.to_owned());
-                window.show();
-            } else {
-                window.hide();
-            }
-            glib::ControlFlow::Continue
-        }));
+    update_window_content(&window, i3_conn.clone());
+
+    let is_visible_clone = is_visible.clone();
+    let i3_conn_clone = i3_conn.clone();
+    glib::timeout_add_local(Duration::from_millis(100), clone!(@weak window => @default-return ControlFlow::Continue, move || {
+        println!("Now is {}", is_visible_clone.load(Ordering::SeqCst));
+        if is_visible_clone.load(Ordering::SeqCst) {
+            update_window_content(&window, i3_conn_clone.to_owned());
+            window.show();
+        } else {
+            window.hide();
+        }
+        glib::ControlFlow::Continue
+    }));
 }
 
+
+extern crate x11;
+
+use std::ptr;
+use x11::xlib;
+
+
 fn update_window_content(window: &ApplicationWindow, i3_conn: Arc<Mutex<I3Connection>>) {
-     let vbox = gtk4::Box::new(gtk4::Orientation::Horizontal, 1);
+    let hbox = gtk4::Box::new(gtk4::Orientation::Horizontal, 1);
+    hbox.set_hexpand(true);
 
-     let mut i3_conn = i3_conn.lock().unwrap();
-     let wks = i3_conn.get_workspaces().unwrap();
+    let mut i3_conn = i3_conn.lock().unwrap();
+    let wks = i3_conn.get_workspaces().unwrap();
 
-     for ws in &wks.workspaces {
-         let button = Button::with_label(&ws.name);
-         button.connect_clicked(|_| {
-             eprintln!("Clicked!");
-         });
+    for ws in &wks.workspaces {
+        let button = Button::with_label(&ws.name);
+        button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
 
-         vbox.append(&button);
-     }
+        hbox.append(&button);
+    }
 
-     window.set_child(Some(&vbox));
+    window.set_child(Some(&hbox));
+
+    // move window to center
+    unsafe {
+        let display = xlib::XOpenDisplay(ptr::null());
+        let screen = xlib::XDefaultScreen(display);
+        let screen_width = xlib::XDisplayWidth(display, screen) as i32;
+        let screen_height = xlib::XDisplayHeight(display, screen) as i32;
+
+        let window_width = window.width();
+        let window_height = window.height();
+
+        let x = (screen_width - window_width) / 2;
+        let y = (screen_height - window_height) / 2;
+
+        let command = format!("[title=\"First GTK Program\"] move window to position {} {}", x, y);
+        i3_conn.run_command(&command).unwrap();
+
+        xlib::XCloseDisplay(display);
+    }
 }
