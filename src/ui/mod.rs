e9ace6da4c9c9770e7955baa19fc865d9e39abf5
Ion Dodon <iondodon2@gmail.com>
2024-03-16 13:37:48 +0200

    refactoring

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -8,12 +8,11 @@ use gdk4::prelude::ApplicationExtManual;
 use gdk4::prelude::DisplayExt;
 use gdk4::prelude::MonitorExt;
 use gtk4::glib::ControlFlow;
-use gtk4::prelude::BoxExt;
 use gtk4::prelude::GtkWindowExt;
 use gtk4::prelude::NativeExt;
 use gtk4::prelude::WidgetExt;
+use gtk4::Application;
 use gtk4::Label;
-use gtk4::{Application, Picture};
 use gtk4::{ApplicationWindow, EventControllerKey};
 use i3ipc::event::inner::WorkspaceChange;
 use i3ipc::event::Event;
@@ -24,109 +23,9 @@ use std::sync::Arc;
 use std::sync::RwLock;
 use std::thread;
 use std::time::Duration;
-use xcap::image::ImageBuffer;
-use xcap::image::Rgba;
 
 mod style;
-
-struct Tab {
-    label: Label,
-    gtk_box: gtk4::Box,
-}
-
-impl Tab {
-    fn new(picture: Option<Picture>, name: Option<&str>) -> Self {
-        let gtk_box = gtk4::Box::new(gtk4::Orientation::Vertical, 1);
-        gtk_box.set_width_request(300);
-        gtk_box.add_css_class("tab");
-
-        if let Some(ref pic) = picture {
-            gtk_box.append(pic);
-        }
-
-        let label = Label::new(name);
-        gtk_box.append(&label);
-
-        Tab {
-            label: Label::new(name),
-            gtk_box: gtk_box,
-        }
-    }
-}
-
-struct TabsList {
-    tabs_box: gtk4::Box,
-    tabs_vec: Vec<Tab>,
-}
-
-impl TabsList {
-    fn new() -> TabsList {
-        let mut tabs = TabsList {
-            tabs_box: gtk4::Box::new(gtk4::Orientation::Horizontal, 3),
-            tabs_vec: Vec::<Tab>::new(),
-        };
-
-        tabs.tabs_box.set_homogeneous(true);
-        tabs.tabs_box.add_css_class("tabs");
-
-        let mut i3_conn_lock = state::I3_CONNECTION.write().unwrap();
-        let wks = i3_conn_lock.get_workspaces().unwrap().workspaces;
-        for (_, ws) in (&wks).iter().enumerate() {
-            tabs.add_new_tab(None, &ws.name);
-        }
-
-        tabs.re_render();
-
-        tabs
-    }
-
-    fn remove_tab(self: &mut Self, name: &String) {
-        for (index, tab) in self.tabs_vec.iter().enumerate() {
-            if tab.label.text().eq(name) {
-                self.tabs_vec.remove(index);
-                return;
-            }
-        }
-    }
-
-    fn add_new_tab(self: &mut Self, picture: Option<Picture>, name: &String) {
-        let tab = Tab::new(picture, Some(name));
-        self.tabs_vec.push(tab);
-    }
-
-    fn re_render(self: &mut Self) {
-        loop {
-            match self.tabs_box.first_child() {
-                Some(tab) => self.tabs_box.remove(&tab),
-                None => break,
-            }
-        }
-
-        for tab in &self.tabs_vec {
-            self.tabs_box.append(&tab.gtk_box);
-        }
-    }
-
-    fn set_image(self: &mut Self, name: String, img: Option<ImageBuffer<Rgba<u8>, Vec<u8>>>) {
-        let mut i: Option<usize> = None;
-        if let Some(pic) = img {
-            for (index, tab) in self.tabs_vec.iter().enumerate() {
-                if tab.label.text().eq(&name) {
-                    i = Some(index);
-                    break;
-                }
-            }
-
-            if let Some(i) = i {
-                let pixbuf = screenshot::rgba_image_to_pixbuf(&pic);
-                let picture = Picture::for_pixbuf(&pixbuf);
-                self.tabs_vec[i] = Tab::new(Some(picture), Some(&name));
-            }
-
-            self.re_render();
-        }
-    }
-}
+mod tabs;
 
 pub fn init() {
     let application = Application::builder()
@@ -147,7 +46,7 @@ fn setup(app: &Application) {
         .css_classes(vec!["window"])
         .build();
 
-    let tabs = Arc::new(RwLock::new(TabsList::new()));
+    let tabs = Arc::new(RwLock::new(tabs::TabsList::new()));
 
     let controller = EventControllerKey::new();
     let window_clone = window.clone();
